f6d7542ddd18e00524606c3cf18f059930506df0
TimurSejati <timur7dian84@gmail.com>
2020-07-06 21:10:22 +0700

    add styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Navbar from "../components/Globals/Navbar"
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
 }
 
 Layout.propTypes = {
